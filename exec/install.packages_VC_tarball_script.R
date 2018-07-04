8fc07fa36affc9042e7798bf58cda25cb2de935f
s.frouws <s.frouws@mcroberts.nl>
2018-07-04 10:53:11 +0200

    renamed and updated

--- exec/install.packages_VC_tarball_script.R
+++ exec/install.packages_VC_tarball_script.R
@@ -7,8 +7,14 @@ if (!file.exists(packagePath)) {
     stop(sprintf("The file (%s) provided does not exist.\nPlease specify 2 arguments.\nThe complete path to the tarball file, and a character string showing the dependencies: `dplyr = (0.5.0), ggplot2 = ()`.", packagePath))
 }
 
-if (!require(RVClibrary)) {
+# Set library
+.libPaths(c(.Library, Sys.getenv("R_LIBS_USER")))
+
+if (suppressMessages(!require(RVClibrary))) {
     stop("\nPlease install this single tarball manually using something like:\n `install.packages('M:/.../RVClibrary_0.1.0.tar.gz', lib = Sys.getenv('R_LIBS_USER'), type = 'source', repos = NULL)`\n\n")
 }
 
-install.packages_VC_tarball(packagePath, dependencies, parse_dependencies = TRUE)
+install.packages_VC_tarball(packagePath, dependencies, execute_with_Rscript = FALSE, parse_dependencies = TRUE)
+
+# Quit the R script execution.
+quit(save = 'no')
